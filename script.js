0dc38e5f98375be70c2e68e4de28a0c1e6d06a75
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-17 21:09:07 +0000

    commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.getElementById("greeting").textContent = "Hi There!";
 document.getElementById("h1").textContent = "the header";
+
+let items = document.getElementsByClassName("redclass");
+for (let i = 0; i < items.length; i++)
+{ 
+    items[i].textcontent = "Text change";
+}
